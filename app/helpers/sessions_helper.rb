8bc58850728d81f19913e8a5db8cabfc2f24abcd
dinh phu <dinhphu94@gmail.com>
2015-12-25 09:54:22 +0700

    finish chapter 9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,22 +14,21 @@ module SessionsHelper
 
   # Returns the user corresponding to the remember token cookie.
   def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      raise       # The tests still pass, so this branch is currently untested.
-
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
+  if (user_id = session[:user_id])
+    @current_user ||= User.find_by(id: user_id)
+  elsif (user_id = cookies.signed[:user_id])
+    user = User.find_by(id: user_id)
+    if user && user.authenticated?(cookies[:remember_token])
+      log_in user
+      @current_user = user
     end
   end
+end
 
   # Returns the current logged-in user (if any).
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+
+  def current_user?(user)
+    user == current_user
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -50,5 +49,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] == request.url if request.get?
   end
 end
