2538f2b98aa536bdc777a6e69ae1b4e16630257b
dinh phu <dinhphu94@gmail.com>
2015-12-30 15:46:17 +0700

    server update

---
+++ config/carrier_wave.rb
@@ -0,0 +1,11 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AKIAJAVHCVYWRFBYB2EA'],
+      :aws_secret_access_key => ENV['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']
+    }
+    config.fog_directory     =  ENV['phu']
+  end
+end
